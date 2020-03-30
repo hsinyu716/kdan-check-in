56c0870daed0a53ed470271e9ad3beccd2789789
Sophia Brandt <git@s-br.net>
2020-03-30 19:21:17 +0200

    🐛 FIX: fix pg connection string for test & dev env

--- node_app/knexfile.js
+++ node_app/knexfile.js
@@ -1,24 +1,24 @@
 // Update with your config settings.
 
 module.exports = {
-  test: {
+  development: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds/test',
+      directory: __dirname + '/db/seeds/development',
     },
   },
-  development: {
+  test: {
     client: 'pg',
     connection: process.env.DATABASE_URL_TEST,
     migrations: {
       directory: __dirname + '/db/migrations',
     },
     seeds: {
-      directory: __dirname + '/db/seeds/development',
+      directory: __dirname + '/db/seeds/test',
     },
   },
   production: {
