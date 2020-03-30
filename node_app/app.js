b0800ce6ec33e1e8450260fb8504d784ffc87e25
Sophia Brandt <git@s-br.net>
2020-03-30 16:38:51 +0200

    👌 IMPROVE: add mocha/chai, change routes

--- node_app/app.js
+++ node_app/app.js
@@ -13,29 +13,42 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.use(logger('dev'))
+if (process.env.NODE_ENV != 'test') {
+  app.use(logger('dev'))
+}
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
+app.get('/', (req, res) => res.redirect('/api/v1/shows'))
+app.use('/api/v1', indexRouter)
 app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404))
 })
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
+// development
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.json({
+      message: err.message,
+      error: err,
+    })
+  })
+}
+// production
+app.use((err, req, res, next) => {
+  // send blank error message
   res.status(err.status || 500)
-  res.render('error')
+  res.json({
+    message: err.message,
+    error: {},
+  })
 })
 
 module.exports = app
